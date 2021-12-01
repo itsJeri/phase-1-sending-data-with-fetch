d147af6d59eb64535617566013c67f241a87ada3
Jerry Tong <zihangtong@gmail.com>
2021-12-01 00:23:59 -0500

    commit tree 01a23918063bdfa0718342ce1024efc6c8db3e44

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(name, email) {
+    const formData = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+            }),
+    };
+
+    fetch('http://localhost:3000/users', formData)
+        .then(res => res.json())
+        .then(function(data) {
+            document.body.append(data.id)
+        })
+        .catch(function(error) {
+            document.querySelector('body').innerText = (error.message)
+        })   
+}
+
+// document.addEventListener('DOMContentLoaded', submitData());
